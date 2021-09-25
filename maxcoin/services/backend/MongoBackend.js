7aea648d3be4da65a021361f73b703f129183089
Jose Estrada <j.estrada49@icloud.com>
2021-09-25 02:42:09 -0700

    Insert data into MongDB

--- maxcoin/services/backend/MongoBackend.js
+++ maxcoin/services/backend/MongoBackend.js
@@ -27,12 +27,22 @@ class MongoBackend {
 
   async disconnect() {
     if(this.client) {
-      return this.cliente.close()
+      return this.client.close()
     }
     return false
   }
 
-  async insert() {}
+  async insert() {
+    const data = await this.coinAPI.fetch()
+    const documents = []
+    Object.entries(data.bpi).forEach((entry) => {
+      documents.push({
+        date: entry[0],
+        value: entry[1]
+      })
+    })
+    return this.collection.insertMany(documents)
+  }
 
   async getMax() {}
 
@@ -47,6 +57,13 @@ class MongoBackend {
       throw new Error('Connecting to MongoDB failed')
     }
     console.timeEnd('mongodb-connect')
+
+    console.info('Inserting into MongoDB')
+    console.time('mongodb-insert')
+    const insertResult = await this.insert()
+    console.timeEnd('mongodb-insert')
+
+    console.info(`Inserted ${insertResult.result.n} documents into MongoDB`)
 
     console.info('Disconnecting from MongoDB')
     console.time('mongodb-disconnect')
