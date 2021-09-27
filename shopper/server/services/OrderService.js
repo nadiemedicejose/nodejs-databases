ffdaa6717d841dc4fbc69abc272b40cf7cf19259
Jose Estrada <j.estrada49@icloud.com>
2021-09-26 18:52:04 -0700

    Create sequelize models

---
+++ shopper/server/services/OrderService.js
@@ -0,0 +1,12 @@
+const Models = require('../models/sequelize')
+
+class OrderService {
+  constructor(sequelize) {
+    Models(sequelize)
+    this.client = sequelize
+    this.models = sequelize.models
+  }
+}
+
+module.exports = OrderService
+
